bf4cebe94e153623288bc95823069043f70dd20c
adithya bandara <Dammikaekanayaka1980@gmail.com>
2024-03-22 22:04:22 +0530

    Clear and Rearranged about things

--- Views/empDashboard.php
+++ Views/empDashboard.php
@@ -22,37 +22,7 @@
 </head>
 <body>
     <h1>Employee Dashboard</h1>
+    <h1>Admin</h1>    
     
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>NIC</th>
-                <th>Employee Number</th>
-                <!-- Add more table headers for other employee properties -->
-            </tr>
-        </thead>
-        <tbody>
-
-            <?php if (!empty($employees)): ?>
-                <?php foreach ($employees as $employee): ?>
-                    <tr>
-                        <td><?php echo $employee->getIdEmployees(); ?></td>
-                        <td><?php echo htmlspecialchars($employee->getFirstName()); ?></td>
-                        <td><?php echo htmlspecialchars($employee->getLastName()); ?></td>
-                        <td><?php echo htmlspecialchars($employee->getNic()); ?></td>
-                        <td><?php echo htmlspecialchars($employee->getEmpNumber()); ?></td>
-                        <!-- Add more table cells for other employee properties -->
-                    </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="5">No employees found</td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
 </body>
 </html>
